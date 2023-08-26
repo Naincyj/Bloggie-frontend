a18ee001638540610fa2c7ba4dcf78db64bfe5c6
Naincy Jain <88013913+Naincyj@users.noreply.github.com>
2023-08-26 18:14:51 +0530

    Update EditPost.jsx

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -14,7 +14,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/'+id)
+    fetch('https://bloggie-bkrf.onrender.com/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
